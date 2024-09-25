1d184a7152f5554621c76def3624e9e59f2c4f5f
Vikram Kumar <vk8969338090@gmail.com>
2024-09-25 09:55:17 +0530

    Data sanitization api

--- app.js
+++ app.js
@@ -202,7 +202,8 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
-
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const globalErrorController = require("./controllers/errorController");
 const AppError = require("./Utils/appError");
 const nftsRouter = require("./routes/nftsRoute");
@@ -211,6 +212,10 @@ const usersRouter = require("./routes/usersRoute");
 const app = express();
 app.use(express.json({ limit: "10kb" }));
 
+// DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+// DATA SANITIZATION AGAINST site script xss
+app.use(xss());
 // SECURE HEADER HTTP
 app.use(helmet());
 // GLOBAL MIDDLEWARE
